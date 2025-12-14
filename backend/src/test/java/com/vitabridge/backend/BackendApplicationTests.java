c21b9dffbd3f4bf6f95142f15c5002a5218716ca
Ruzhan Reaz <154721665+ruzhanreaz@users.noreply.github.com>
2025-12-14 08:11:13 +0600

    created backend files

---
+++ backend/src/test/java/com/vitabridge/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitabridge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
